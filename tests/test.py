e7bea1ea844ba3740b062747e586b9f4f4ab0c16
‘‘Sigure’ <sigure_mo@163.com>
2018-08-03 12:14:45 +0800

    新增字幕类型的下载

--- tests/test.py
+++
@@ -1,8 +0,0 @@
-from Easyload import *
-from siguretools import config
-config=Config('data/config.txt','$')
-mob_token=gettoken(config.username,config.passwd)[0]
-root=config.get(platform.system()+'Root')
-courseinfo=get_courseinfo(1002777001,mob_token)
-print(courseinfo)
-input()
