19f265d856eecc2ee60c9733a0ecfc6028fa8cde
SigureMo <sigure_mo@163.com>
2018-10-26 13:04:53 +0800

    :bug: Fix bug

--- tools/playList.py
+++ tools/playList.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 
 root = '.'
@@ -14,22 +13,3 @@ for dirpath, dirnames, filenames in os.walk(root):
 with open(root+os.sep+'Playlist.m3u','w',encoding='utf-8') as f:
     f.write(s.replace('\\', '/'))
 input('已成功生成播放列表')
-        
-=======
-import os
-
-root = '.'
-mode = 'RP'
-s = ''
-for dirpath, dirnames, filenames in os.walk(root):
-    for filename in filenames:
-        if os.path.splitext(filename)[-1] in ['.mp4', '.flv', '.rm', '.rmvb', '.3gp', '.avi', '.mpeg', '.mpg', '.mkv', '.dat', ',asf', '.wmv', '.mov', '.ogg', '.ogm',]:
-            path = dirpath + os.sep + filename + '\n'
-            if mode == 'RP':
-                path = path.replace(root + os.sep, '')
-            s += path
-with open(root+os.sep+'Playlist.m3u','w',encoding='utf-8') as f:
-    f.write(s.replace('\\', '/'))
-input('已成功生成播放列表')
-        
->>>>>>> history
